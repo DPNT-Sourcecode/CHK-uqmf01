fda0da567a775373c5b543cf2a84f7beb39d1ff0
sbx_user1061 <sbx_user1061@ip-10-12-179-238.eu-west-2.compute.internal>
2018-06-23 14:38:19 +0000

    Sat Jun 23 14:38:19 UTC 2018

---
+++ src/BeFaster.App/Solutions/Checkout/SkuInvalidException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BeFaster.App.Solutions.Checkout
+{
+    public class SkuInvalidException : Exception
+    {
+        public SkuInvalidException(char sku) 
+            : base($"The SKU '{sku}' is invalid") { }
+    }
+}
