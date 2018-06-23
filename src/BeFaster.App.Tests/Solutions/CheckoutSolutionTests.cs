7e43e84dd1c40a704670cea52cf44a5b5687f2a1
sbx_user1061 <sbx_user1061@ip-10-12-179-238.eu-west-2.compute.internal>
2018-06-23 14:44:19 +0000

    Sat Jun 23 14:44:19 UTC 2018

--- src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
@@ -31,4 +31,20 @@ namespace BeFaster.App.Tests.Solutions
             Assert.That(checkoutPricer.CalculatePrice("X"), Is.EqualTo(10));
         }
     }
+
+    [TestFixture]
+    class WhenBuyingTwoDifferentItems
+    {
+        [Test]
+        public void ThePriceIsTheSumOfTheTwoSkuPrices()
+        {
+            var priceDatabase = new Mock<IPriceDatabase>();
+            priceDatabase.Setup(x => x.GetIndividualPriceFor('X'))
+                .Returns(10);
+            priceDatabase.Setup(x => x.GetIndividualPriceFor('Y'))
+                .Returns(5);
+            var checkoutPricer = new CheckoutPricer(priceDatabase.Object);
+            Assert.That(checkoutPricer.CalculatePrice("XY"), Is.EqualTo(15));
+        }
+    }
 }
