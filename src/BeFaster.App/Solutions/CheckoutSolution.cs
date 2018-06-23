bb708c89ca60fa2ca8668ecd1deff45e1c50ad22
sbx_user1061 <sbx_user1061@ip-10-12-179-238.eu-west-2.compute.internal>
2018-06-23 15:26:19 +0000

    Sat Jun 23 15:26:19 UTC 2018

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -1,4 +1,6 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using BeFaster.App.Solutions.Checkout;
+using BeFaster.Runner.Exceptions;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions
 {
@@ -6,7 +8,39 @@ namespace BeFaster.App.Solutions
     {
         public static int Checkout(string skus)
         {
-            throw new SolutionNotImplementedException();
+            var checkoutPricer = new CheckoutPricer(
+                new InMemoryPriceDatabase());
+        }
+
+        private class InMemoryPriceDatabase : IPriceDatabase
+        {
+            private readonly Dictionary<char, int> individualPrices;
+            private readonly Dictionary<char, MultiPrice> multiPrices;
+
+            public InMemoryPriceDatabase()
+            {
+                individualPrices = new Dictionary<char, int>
+                {
+                    { 'A', 50 },
+                    { 'B', 30 },
+                    { 'C', 20 },
+                    { 'D', 15 },
+                };
+                multiPrices = new Dictionary<char, MultiPrice>
+                {
+                    {'A', new MultiPrice(quantity: 3, price: 130) },
+                    {'B', new MultiPrice(quantity: 2, price: 45) },
+                };
+            }
+            public int GetIndividualPriceFor(char sku)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public MultiPrice GetMultiPriceOfferFor(char sku)
+            {
+                throw new System.NotImplementedException();
+            }
         }
     }
 }
