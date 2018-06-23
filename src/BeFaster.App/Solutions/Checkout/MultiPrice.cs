a5ab7bc361e0ae3a1b590c68266da02adbfc45fd
sbx_user1061 <sbx_user1061@ip-10-12-179-238.eu-west-2.compute.internal>
2018-06-23 14:53:19 +0000

    Sat Jun 23 14:53:19 UTC 2018

---
+++ src/BeFaster.App/Solutions/Checkout/MultiPrice.cs
@@ -0,0 +1,17 @@
+﻿namespace BeFaster.App.Solutions.Checkout
+{
+    /// <summary>
+    /// Represents an offer where if you buy this Quantity of items, the total price will be Price
+    /// </summary>
+    public class MultiPrice
+    {
+        public MultiPrice(int quantity, int price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public int Quantity { get; }
+        public int Price { get; }
+    }
+}
